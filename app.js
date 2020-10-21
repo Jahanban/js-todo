db626d91aaf5fe560398e15fc58d2c9fb03d42aa
Jahanban <jahanban@gmail.com>
2020-10-21 16:35:45 -0400

    Added filter function and filter event listener

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 //Functions
 function addTodo(event) {
@@ -56,4 +58,11 @@ function deleteCheck(e) {
         const todo = item.parentElement;
         todo.classList.toggle("completed");
     }
+}
+
+// Create function for a filter
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    console.log(todos);
 }
